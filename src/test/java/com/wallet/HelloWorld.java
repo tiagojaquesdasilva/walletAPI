13b69e3091f05fda50b86cff3017ae9f70a40aaa
cwi.tjaques <cwi.tjaques@bv.com.br>
2023-03-04 15:19:42 -0300

    Primeiro Test para verificar se esta rodando corretamente e configurações.

---
+++ src/test/java/com/wallet/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.wallet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloWorld {
+
+    @Test
+    public void testHelloWord() {
+        assertEquals(1,1);
+    }
+}
